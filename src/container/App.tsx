23d7b8758e3bdbd3cd6f77be76bbcf6943ac57b2
PachVerb <1506262681@qq.com>
2020-10-04 20:12:42 +0800

    feat: 添加布局

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.styl';
+
 
 function App() {
   return (
     <div className="container">
-      
+      <div className="content">
+        
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {BrowserRouter, Switch} from 'react-router'; // 导入路由
+// import {Router, Route} from 'react-router'
 import './css/index.styl';
 import App from './container/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+
+ReactDOM.render((
+<App />),
   document.getElementById('root')
 );
