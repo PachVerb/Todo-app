ac57410e6a6f33dbd6dfa4ca80b9ab0886b0be76
PachVerb <1506262681@qq.com>
2020-10-05 20:41:17 +0800

    style: 调整布局代码

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,15 +1,25 @@
 import React, {FC} from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Input } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 import './App.css';
-
 
 const App: FC = () => (
     <div className="container"> 
-      <Row>
-        <Col span={12} offset={6}>
-          col-6 col-offset-6
-        </Col>
-      </Row>
+      <div className="content">
+        <Row>
+          <Col span={24}>
+            <div style={{ marginBottom: 16 }}>
+              <Input addonAfter={<PlusOutlined />} defaultValue="mysite" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={24}>col</Col>
+        </Row>
+        <Row>
+          <Col span={24}>col</Col>
+        </Row>
+      </div>
     </div>
 )
 
