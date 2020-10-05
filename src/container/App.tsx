934e909120c773340d2c1f3e7233185d6e234b51
PachVerb <1506262681@qq.com>
2020-10-05 19:59:30 +0800

    feat: 添加布局

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import './App.styl';
+import React, {FC} from 'react';
+import { Row, Col } from 'antd';
+import './App.css';
 
 
-function App() {
-  return (
-    <div className="container">
-      <div className="content">
-        
-      </div>
+const App: FC = () => (
+    <div className="container"> 
+      <Row>
+        <Col span={12} offset={6}>
+          col-6 col-offset-6
+        </Col>
+      </Row>
     </div>
-  );
-}
+)
 
 export default App;
